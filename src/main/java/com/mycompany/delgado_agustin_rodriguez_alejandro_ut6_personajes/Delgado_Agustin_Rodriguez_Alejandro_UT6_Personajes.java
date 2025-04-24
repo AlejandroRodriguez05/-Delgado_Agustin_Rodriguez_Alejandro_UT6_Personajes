6ebce952720124343526380570cf0ba1281ab97a
Agustin <agustindelgadoestevez@gmail.com>
2025-04-24 18:27:35 +0100

    proyecto

---
+++ src/main/java/com/mycompany/delgado_agustin_rodriguez_alejandro_ut6_personajes/Delgado_Agustin_Rodriguez_Alejandro_UT6_Personajes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.delgado_agustin_rodriguez_alejandro_ut6_personajes;
+
+/**
+ *
+ * @author AlumnadoTarde
+ */
+public class Delgado_Agustin_Rodriguez_Alejandro_UT6_Personajes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
